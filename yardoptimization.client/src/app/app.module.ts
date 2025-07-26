58ff7667b1a12ca8644f69b902e4c07887abe3ec
Akmal Fazili <92304771+Akmalfazili@users.noreply.github.com>
2025-07-27 01:53:22 +0800

    initial setup 26/7/25

--- yardoptimization.client/src/app/app.module.ts
+++ yardoptimization.client/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppComponent } from './app.component';
 
-import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
     HomeComponent,
     NavMenuComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
+    BrowserModule, HttpClientModule, BrowserAnimationsModule,
     AppRoutingModule, MatButtonModule, MatIconModule, MatToolbarModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
